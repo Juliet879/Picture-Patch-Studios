9a1fea323b96a8698c28d6c33de919a3d6de254c
JulietK <julietgisemba000@>
2020-08-06 19:48:46 +0300

    add final comits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$("#mc-embedded-subscribe").click(function(event){
+$("#submit").click(function(event){
     var name= $("input[type='text']").val();
     alert("Thank you"+" "+name+" "+"for the input. We'll be sure to take into account.")
 })
