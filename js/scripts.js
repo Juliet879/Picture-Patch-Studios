607e71399c6eb8d63f49a38ab28b6e0bb99aaf1a
JulietK <julietgisemba000@>
2020-08-06 15:05:47 +0300

    add the mailchimp

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+$("#mc-embedded-subscribe").click(function(event){
+    var name= $("input[type='text']").val();
+    alert("Thank you"+" "+name+" "+"for the input. We'll be sure to take into account.")
+})
